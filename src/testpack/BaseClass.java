661c7279f46f42d607400010ec5fd0f686fabfea
hemajaikumar <hema.jaikumar2004@gmail.com>
2018-10-24 12:56:55 +0530

    Added method to add Numbers

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -5,9 +5,17 @@ public class BaseClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
+		addNumber();
 	}
 
 	public static void add() {
 		System.out.println("some code added by hema");
+	}
+	
+	public static void addNumber(){
+		int a = 2;
+		int b = 5;
+		System.out.println("Addition of two numbers :" +(a+b));
+		
 	}
 }
