c5eb7d1b9ff51d585baa68b44444d722e7aeb283
vickychennai <vigsan2008@gmail.com>
2018-10-25 10:49:41 +0530

    changes made

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -5,6 +5,7 @@ public class TestCase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("added test case1");
+		System.out.println("Changes made");
 	}
 
 }
