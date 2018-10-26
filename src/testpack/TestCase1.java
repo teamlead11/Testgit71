76e81d0f4a597bd54a76c5d19f3ca0c4525b3ef9
JayiD07 <jayidas1993@gmail.com>
2018-10-26 11:28:30 +0530

    reverse string

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -1,11 +1,15 @@
 package testpack;
 
+import java.util.Scanner;
+
 public class TestCase1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("added test case1");
 		System.out.println("Changes made");
+		TestCase1 t=new TestCase1();
+		t.reverseByname();
 	}
 
 	public static void multi(int a, int b) {
@@ -16,4 +20,16 @@ public class TestCase1 {
 		System.out.println(a * b * c);
 
 	}
-}
+	private void reverseByname() {
+		String original,reverse="";
+		Scanner in =new Scanner(System.in);
+		System.out.println("Enter a string to reverse");
+		original = in.nextLine();
+		int length = original.length();
+		for (int i = length - 1; i >= 0; i--)
+		reverse = reverse + original.charAt(i);
+		System.out.println("Reverse of entered string is: " + reverse);
+		}
+
+	}
+
