3ce7ec23fa007d2d04f65d64395af9fc06ef961f
sindhu1114 <sindhumanu1114@gmail.com>
2018-10-26 11:34:09 +0530

    changes made by sindhu

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -1,4 +1,5 @@
 package testpack;
+import java.util.Scanner;
 
 public class TestCase1 {
 
@@ -6,6 +7,8 @@ public class TestCase1 {
 		// TODO Auto-generated method stub
 		System.out.println("added test case1");
 		System.out.println("Changes made");
+		TestCase1 tc=new TestCase1();
+		tc.reversebysindhu();
 	}
 
 	public static void multi(int a, int b) {
@@ -16,4 +19,16 @@ public class TestCase1 {
 		System.out.println(a * b * c);
 
 	}
+ public void reversebysindhu() {
+	String original, reverse = "";
+	Scanner in = new Scanner(System.in);
+	System.out.println("Enter a string to reverse");
+	original = in.nextLine();
+	int length = original.length();
+	for (int i = length - 1; i >= 0; i--)
+	reverse = reverse + original.charAt(i);
+	System.out.println("Reverse of entered string is: " + reverse);
+	}
+	
+	
 }
