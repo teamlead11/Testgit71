e130cc87b1c1cd8bd04e92f9606256bafe612884
banumathi07 <banumathisekar.94@gmail.com>
2018-10-26 18:59:30 +0530

    banutest changes

--- src/testpack/TestCase1.java
+++ src/testpack/TestCase1.java
@@ -41,4 +41,15 @@ public class TestCase1 {
 			reverse = reverse + original.charAt(i);
 		System.out.println("Reverse of entered string is: " + reverse);
 	}
+	
+	public void reverseByBanu() {
+		String original, reverse = "";
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a string to reverse");
+		original = in.nextLine();
+		int length = original.length();
+		for (int i = length - 1; i >= 0; i--)
+			reverse = reverse + original.charAt(i);
+		System.out.println("Reverse of entered string is: " + reverse);
+	}
 }
