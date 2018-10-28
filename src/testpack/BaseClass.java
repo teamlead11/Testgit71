20c0526105c7e25a08a121ebd851f6f56a45efa1
dinadas <dkg031292@gmail.com>
2018-10-29 04:47:10 +0530

    Done

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -3,10 +3,11 @@ package testpack;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class BaseClass {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my base class");
 		addNumber();
@@ -33,9 +34,60 @@ public class BaseClass {
 		int b = 5;
 		System.out.println("Addition of two numbers :" +(a+b));
 		
+	}*/ 
+	public static class ReverseEveryWordOfGroupOfString {
+
+		/*
+		 * This method splits given string by space delimiter and returns a String
+		 * array.
+		 */
+		public static String[] splitStringBySpace(String inputStringToSplit) {
+			return inputStringToSplit.split(" ");
+		}
+
+		/*
+		 * This method reverse the given word and return it.
+		 */
+		public static String reverseWord(String inputWord) {
+			String reverse = "";
+			// Reading char by char from end and appending
+			for (int i = inputWord.length() - 1; i >= 0; i--) {
+				reverse = reverse + inputWord.charAt(i);
+			}
+			return reverse;
+		}
+
+		public static void main(String[] args) {
+
+			// User input for the string to know length
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Please enter the string to reverse each word:");
+
+			String userInput = sc.nextLine();
+
+			sc.close();
+			
+			// Split string
+			String[] splitedString = splitStringBySpace(userInput);
+			
+			// Output with each word reversed
+			String desiredString="";
+			
+			// Reversing word by word and appending
+			for(int j=0;j<splitedString.length;j++)
+			{
+				desiredString= desiredString + reverseWord(splitedString[j]) +" ";
+			}
+			
+			System.out.println("Original string            :"+userInput);
+			
+			System.out.println("String with reversed words :"+desiredString);
+		}
 	}
 	
-   private static void reversebyvicky() {
+	
+	
+   /*private static void reversebyvicky() {
 	   Scanner inp = new Scanner(System.in);
 		int number, reverse = 0;
 		System.out.println("Enter the value that you have to reverse: ");
@@ -66,7 +118,7 @@ public class BaseClass {
 			   System.out.println("n is not a palindrome number");
 		   }
 		   
-	   }
+	   }*/
  		
  	
  		
